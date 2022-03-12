f023860fabf902e223ece24ed0d5b92034ca56f9
bert <1163236754@qq.com>
2022-03-13 01:59:57 +0800

    添加二叉树中序遍历

---
+++ src/tree/InorderTraversal.java
@@ -0,0 +1,40 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InorderTraversal {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    // 堆栈版本待更新
+    public List<Integer> inorderWithStack(TreeNode root) {
+
+        return new ArrayList<>();
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        return list;
+    }
+
+    public void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
