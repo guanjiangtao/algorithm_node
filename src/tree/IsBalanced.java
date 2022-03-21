ce75d8fd218800284358488e5522f3d9a92b48b2
bert <1163236754@qq.com>
2022-03-21 09:27:46 +0800

    更新平衡二叉树算法

--- src/tree/IsBalanced.java
+++ src/tree/IsBalanced.java
@@ -22,4 +22,22 @@ public class IsBalanced {
         int right = height(root.right);
         return Math.max(left, right) + 1;
     }
+
+    public boolean isBalanced1(TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+
+        return Math.abs(height1(root.left) - height1(root.right)) <= 1 && isBalanced1(root.left) && isBalanced1(root.right);
+    }
+
+    public int height1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = height1(root.left);
+        int right = height1(root.right);
+
+        return Math.max(left, right) + 1;
+    }
 }
